bc9395f23aeb89d138c9796b29ce94f5ff3afe0c
msafar024 <msafar024@gmail.com>
2023-05-26 18:06:15 -0500

    CONTACT

--- spectre-news/src/pages/Contact.js
+++ spectre-news/src/pages/Contact.js
@@ -10,17 +10,13 @@ function Contact()
             
             <div className="panel-teschner">
                 <h2>Michael Teschner</h2>
-                <p>Lorem ipsum dolor sit amet. Qui fugiat sunt cum incidunt magni aut praesentium quae eum ratione dignissimos non nisi consequuntur et eligendi libero. Et quod explicabo 33 consequatur sapiente aut vitae voluptas qui dolore consequatur qui fuga quam hic dignissimos reiciendis quo aspernatur dolores? Est blanditiis optio qui saepe error et eius obcaecati. Sed voluptas labore ut adipisci ipsam vel dignissimos veniam quo voluptate sunt?
-                    Ut voluptatibus voluptas et doloremque soluta eos dolorum sunt non reprehenderit eius. Ea illo itaque eum ipsum voluptatem id dolor veniam qui molestias sint. Ex quibusdam voluptas ut autem galisum aut repellat beatae! Sed perferendis repellendus ab voluptatem quas ut ullam atque est repellat magni!
-                    Est impedit mollitia ea saepe quibusdam rem velit fugiat et nihil velit ut expedita totam At labore quia. Sit quia magni ut nihil nihil vel soluta inventore et ipsam galisum qui neque incidunt est sint consequuntur ut aliquid ipsa.</p>
+                <p>A developer of German origin. One of the Co-founders of SpectreNews.</p>
                 <p>reach me at: mteschner@spectrenews.com</p>
             </div>
 
             <div className="panel-safar">
                 <h2>Michael Safar</h2>
-                <p>Lorem ipsum dolor sit amet. Qui fugiat sunt cum incidunt magni aut praesentium quae eum ratione dignissimos non nisi consequuntur et eligendi libero. Et quod explicabo 33 consequatur sapiente aut vitae voluptas qui dolore consequatur qui fuga quam hic dignissimos reiciendis quo aspernatur dolores? Est blanditiis optio qui saepe error et eius obcaecati. Sed voluptas labore ut adipisci ipsam vel dignissimos veniam quo voluptate sunt?
-                    Ut voluptatibus voluptas et doloremque soluta eos dolorum sunt non reprehenderit eius. Ea illo itaque eum ipsum voluptatem id dolor veniam qui molestias sint. Ex quibusdam voluptas ut autem galisum aut repellat beatae! Sed perferendis repellendus ab voluptatem quas ut ullam atque est repellat magni!
-                    Est impedit mollitia ea saepe quibusdam rem velit fugiat et nihil velit ut expedita totam At labore quia. Sit quia magni ut nihil nihil vel soluta inventore et ipsam galisum qui neque incidunt est sint consequuntur ut aliquid ipsa.</p>
+                <p>A developer based out of St. Louis, MO. One of the Co-founders of SpectreNews. </p>
                 <p>reach me at: michaelsafar@spectrenews.com</p>       
             </div>
             
