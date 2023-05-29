91d48fd37efacb6a41c088e759d8a0396f8fe23d
x32dev <120539187+FlexTechDev@users.noreply.github.com>
2023-05-29 17:49:38 -0600

    html parser

--- spectre-news/src/panels/Panel.js
+++ spectre-news/src/panels/Panel.js
@@ -1,15 +1,16 @@
 import React from "react";
-import styles from "./Panel.css";
+import "./Panel.css";
+import ReactHtmlParser from 'html-react-parser';
 
-const Panel = ({ imageUrl, title, content, url }) => {
+const Panel = ({ imageUrl, title, content, url }) => {  
   return (
     <div style={{width:"250px", height:"350px"}} className="panel">
       {imageUrl && (
-        <img className="panel-image" src={imageUrl} alt={title} />
+        <img className="panel-image" src={ReactHtmlParser(imageUrl)} alt={title} />
       )}
       <div className="container">
-        <h2 style={{fontSize:"15px"}}>{title}</h2>
-        <p style={{fontSize:"10px"}}>{content}</p>
+        <h2 style={{fontSize:"15px"}}>{ReactHtmlParser(title)}</h2>
+        <p style={{fontSize:"10px"}}>{ReactHtmlParser(content)}</p>
         <a className="read-more-button" href={url} target="_blank" rel="noopener noreferrer">
           Read more
         </a>
