d707e285414e8f62a45a50333b6c56eef0889c41
msafar024 <msafar024@gmail.com>
2023-05-29 22:24:51 -0500

    YESSSSS W HOME SCREEN

--- spectre-news/src/pages/Home.js
+++ spectre-news/src/pages/Home.js
@@ -4,14 +4,20 @@ import "./Home.css";
 import { FiArrowRightCircle } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
+const backgroundVideo = process.env.PUBLIC_URL + '/Moving.mp4';
+
 function Home() {
   return (
     <div className="home-container">
+      <video autoPlay loop muted className="background-video">
+        <source src={backgroundVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <Bar search={false} />
       <div className="content">
         <h1>Welcome to SpectreNews</h1>
         <p>
-          SpectreNews is your go-to source for the latest and greatest news from around the world. Our team of dedicated developers has worked tirelessly to bring you the most important stories and insightful analysis, helping to filter the left, right, and neutral political biases for <strong>YOU</strong>.
+        SpectreNews is your go-to source for the latest and greatest news from around the world. Our team of dedicated developers has worked tirelessly to bring you the most important stories and insightful analysis, helping to filter the left, right, and neutral political biases for <strong>YOU</strong>.
         </p>
         <Link to="/news" className="cta-button">
           Get Started <FiArrowRightCircle className="icon" />
