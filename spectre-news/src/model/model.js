7308609bee9bcac848a9a4c05e4c931ea5f3c867
msafar024 <msafar024@gmail.com>
2023-06-14 13:16:23 -0500

    Tesnor

--- spectre-news/src/model/model.js
+++ spectre-news/src/model/model.js
@@ -1,27 +1,73 @@
-import * as tf from '@tensorflow/tfjs'
+import Parser from 'rss-parser';
+import * as tf from '@tensorflow/tfjs';
 
-class Model
-{
-    init()
-    {
-        self.model = tf.sequential()
-        model.add(tf.layers.dense({units:1,inputShape:[1]}))
-        
-        model.compile({loss: 'meanSquaredError', optimizer: 'sgd'})
-        
-        const xs = tf.tensor2d([1,2,3,4], [4,1])
-        const ys = tf.tensor2d([1,3,5,7], [4,1])
-    }
+let parser = new Parser();
 
-    train()
-    {
-        model.fit(xs, ys, {epochs:10}).then(() => {
-            model.predict(tf.tensor2d([5], [1,1])).print()
-        })
-    }
+// Define your feed URLs
+let feedUrls = [
+    "https://rss.app/feeds/uMynjxfieM4YYyOR.xml",
+    "https://rss.app/feeds/IrR1trjeWth5flf3.xml",
+    "https://rss.app/feeds/HwQ54e6Y1E4Lq0JR.xml",
+    "https://rss.app/feeds/nlYoBC4pf0j1tvwm.xml",
+    "https://rss.app/feeds/9hoVR8LmiMTw1Iuq.xml",
+    "https://rss.app/feeds/WsBrDv9RwmtTo6rk.xml",
+    "https://rss.app/feeds/hLMACAaZXHNlarCM.xml",
+    "https://rss.app/feeds/ai2qIa6ptsuv3HpX.xml",
+    "https://rss.app/feeds/8hG54V6SZVgtASGm.xml",
+    "https://rss.app/feeds/jL7bEBczL95um3zT.xml",
+    "https://rss.app/feeds/yCv6rlOC1tXpGnjJ.xml",
+    "https://rss.app/feeds/yphrbrN4e7iEOLGb.xml",
+    "https://rss.app/feeds/cSIKEOsVnEntOkbo.xml",
+    "https://rss.app/feeds/PCtv5x4ZhZ86M4fx.xml",
+    "https://rss.app/feeds/4saEdrwdlWPSzRQs.xml",
+    "https://rss.app/feeds/ZHNLLb0R7PV8eJLO.xml",
+    "https://rss.app/feeds/igrhPJAU5sfit0a4.xml",
+    "https://rss.app/feeds/S70Xb47GWcXbS4zN.xml",
+    "https://rss.app/feeds/IDnv0TpWiNASL0rq.xml",
+    "https://rss.app/feeds/qO1DQHYV7mMjui95.xml",
+    "https://rss.app/feeds/unPCxuflO9qJJ6hR.xml",
+    "https://rss.app/feeds/i9F3VBuUbWCKZffM.xml",
+    "https://rss.app/feeds/5t4s1q1dBeNHWdiH.xml",
+    "https://rss.app/feeds/2xZFLvUhZWXDcMHz.xml",
+    "https://rss.app/feeds/p5nAgrpVjy50N642.xml",
+    "https://rss.app/feeds/14JmoWDCYvrxUu3F.xml",
+    "https://rss.app/feeds/VcyYpiTPmxw6Bo1z.xml",
+    "https://rss.app/feeds/wWUxeYBaN4pPjeCs.xml",
+    "https://rss.app/feeds/4QeJZenYqueRNSft.xml",
+    "https://rss.app/feeds/4FTw0jY3aQpDhThY.xml",
+    "https://rss.app/feeds/fYMZ4wDAkQheoQVt.xml",
+    "https://rss.app/feeds/KKq2cccXzGoBNliv.xml",
+    "https://rss.app/feeds/Qay8k9gPLgR0xRBU.xml",
+    "https://rss.app/feeds/QYsSPqWFzoEU9PYn.xml",
+];
 
-    predict()
-    {
-        model.predict(tf.tensor2d([5], [1,1]))
+let model;
+
+// Load the model
+tf.loadLayersModel('path/to/my/model.json').then(loadedModel => {
+  model = loadedModel;
+
+  let feedPromises = feedUrls.map(url => parser.parseURL(url));
+
+  Promise.all(feedPromises).then(feeds => {
+    for(let feed of feeds) {
+      // 2. Extract and pre-process text data
+      let preProcess = (text) => text;
+
+      // 3. Make predictions
+      for(let item of feed.items) {
+        let text = preProcess(item.content);
+        let tensor = tf.tensor([text]); // Convert to tensor
+        let prediction = model.predict(tensor);
+        item.predictedClass = ['left', 'center', 'right'][prediction.argMax(-1).dataSync()[0]];
+      }
+
+      // 4. Sort by predicted class
+      let leftItems = feed.items.filter(item => item.predictedClass === 'left');
+      let centerItems = feed.items.filter(item => item.predictedClass === 'center');
+      let rightItems = feed.items.filter(item => item.predictedClass === 'right');
+
+      // Do something with leftItems, centerItems, and rightItems
     }
-}
+  });
+});
